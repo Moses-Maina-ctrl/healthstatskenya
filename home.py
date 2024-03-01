5a97f48a6747b78a10d2a4c5c656d9afdffac74d
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-03-01 17:09:29 +0300

    Home.py

--- home.py
+++ home.py
@@ -1,3 +1,23 @@
 import streamlit as st
+from st_pages import Page, show_pages, add_page_title
 
-st.write("Hey Yaaa")
+kenyan_flag = "\U0001F1F0\U0001F1EA"
+
+def main():
+    st.title(f'Health Stats {kenyan_flag}')
+    st.markdown(
+        """
+        Welcome to Health Stats Kenya, a Data Visualization  Site that explores the spread and impact of diseases in different regions of Kenya.
+
+
+
+        """
+    )
+
+st.set_page_config(
+    page_title ="HealthStats",
+    page_icon="HS"
+)
+
+if __name__ == "__main__":
+    main()
