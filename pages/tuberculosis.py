7822dd49bbf1f8fabdecdb36ce9040547cf7e379
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-03-01 17:01:55 +0300

    Create files

---
+++ pages/tuberculosis.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+
+url = "https://data.humdata.org/dataset/accb9f02-ed55-460b-b7a2-534b59b5f27b/resource/573f09bd-084f-4e51-8936-8a40048b7c94/download/kenya-tuberculosis-prevalence-per-county.csv"
+data = pd.read_csv(url)
+df= pd.DataFrame(data)
+df2=df.dropna()
+print(df2)
