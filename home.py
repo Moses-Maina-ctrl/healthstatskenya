d4604754474270148923f798c2a7ff9ad6a2756f
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-03-06 18:25:08 +0300

    buttons

--- home.py
+++ home.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_extras.metric_cards import style_metric_cards
 from streamlit_extras.stoggle import stoggle
+from st_pages import Page, Section, show_pages, add_page_title
+
 
 kenyan_flag = "\U0001F1F0\U0001F1EA"
 
@@ -28,7 +30,7 @@ def main():
     st.metric('Infant Mortality Rate','30/1000 live births')
     stoggle('Source','https://www.cdc.gov/globalhealth/countries/kenya')
     style_metric_cards(background_color="#0e1117")
-    
+    st.link_button("Get Informed About Infectious Diseases",url="/1_infectiousDiseases.py") 
         
 
 st.set_page_config(
@@ -38,3 +40,11 @@ st.set_page_config(
 
 if __name__ == "__main__":
     main()
+
+show_pages(
+    [
+        Page("home.py", "Home", "🏠"),
+        Section("Infectious Diseases"),
+        Page("pages/1_infectiousDiseases.py", "Infectious Diseases")
+    ]
+) 
